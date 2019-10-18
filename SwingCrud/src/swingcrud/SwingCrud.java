0940f66b574d02755c3ff33b524bb95017c3fc6f
Yudanis Taqwin Rohman <danistus3@gmail.com>
2019-10-18 15:02:28 +0700

    crud template

---
+++ SwingCrud/src/swingcrud/SwingCrud.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swingcrud;
+
+/**
+ *
+ * @author ytr
+ */
+public class SwingCrud {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
